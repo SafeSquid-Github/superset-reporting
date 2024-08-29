971fa3264be8ab46caa936f670a86be3835604e9
Pratik-Poojary <pratik@safesquid.net>
2024-08-29 11:39:58 +0000

    adding name for the rsyslog actions

--- scripts/log_rotate.sh
+++ scripts/log_rotate.sh
@@ -3,23 +3,24 @@
 # Directory passed as an argument
 BASE_DIR="${1}"
 LOG="${2}"
+[ "${#}" == '0' ] && echo "ERROR: No Input" exit 1
 SCRIPT_DIR="/opt/aggregator/aggregator"
 PY_INSERT="main.py"
 LOG_FILE_NAME="${LOG}.log"
 OUT_FILE="/tmp/.${LOG}_list"
 
 # Find extended log files and process them
-for LOGFILE in "${BASE_DIR}/*/*/${LOG_FILE_NAME}"
+for LOGFILE in "${BASE_DIR}/${LOG}/*/${LOG_FILE_NAME}"
 do
-    LOG_DIR="$(dirname "$LOGFILE")"
+    LOG_DIR="$(dirname "${LOGFILE}")"
     NOW="$(date +"%Y%m%d%H%M%S")"
     MAX_SIZE="$((100 * 1024 * 1024))" #100MB
 
     # Check if the log file exists
-    [ ! -f "${LOGFILE}" ] && continue
+    [ ! -f ${LOGFILE} ] && continue
 
     # Get the current size
-    CURRENT_SIZE="$(stat -c %s "${LOGFILE}")"
+    CURRENT_SIZE="$(stat -c %s ${LOGFILE})"
 
     [ "${CURRENT_SIZE}" -le "${MAX_SIZE}" ] && continue
 
